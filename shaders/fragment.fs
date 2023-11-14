a97bb6e1c18593c615d228a261243a8cc0d5dfef
Zoheb Shujauddin <zoheb2424@gmail.com>
2023-11-14 16:17:55 +0530

    exercise: flips smiley texture horizontally through fragment shader

--- shaders/fragment.fs
+++ shaders/fragment.fs
@@ -12,10 +12,15 @@ in vec2 TextureCoord;
 out vec4 outColor;
 
 void main() {
+
+    vec2 hrzntlFlipTextureCoord = vec2(
+        1.0 - TextureCoord.x,
+        TextureCoord.y
+    );
     // Color output is determined by vertex
     outColor = mix(
         texture(texture1, TextureCoord),
-        texture(texture2, TextureCoord),
+        texture(texture2, hrzntlFlipTextureCoord),
         0.2 // 80% of the first texture, and 20% of the second
     );
 }
