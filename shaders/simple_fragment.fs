5218fc79f4b4ff88e78652952270a8f87d495319
Zoheb Shujauddin <zoheb2424@gmail.com>
2024-01-07 16:32:58 +0530

    implements colour inversion with a single line change

--- shaders/simple_fragment.fs
+++ shaders/simple_fragment.fs
@@ -7,5 +7,5 @@ in vec2 TextureCoord;
 out vec4 FragColor;
 
 void main() {
-    FragColor = texture(screenTexture, TextureCoord);
+    FragColor = vec4(vec3(1.0 - texture(screenTexture, TextureCoord)), 1.0);
 }
