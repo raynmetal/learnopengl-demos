5f957c19f99b682f3c58022e05a7d5290d3d4b0f
Zoheb Shujauddin <zoheb2424@gmail.com>
2024-01-07 17:28:35 +0530

    implements blur kernel effect

--- main.cpp
+++ main.cpp
@@ -50,8 +50,8 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    Shader colorsharpenShader{"shaders/simple_vertex.vs", "shaders/colorsharper_fragment.fs"};
-    if(!colorsharpenShader.getBuildSuccess()) {
+    Shader blurShader{"shaders/simple_vertex.vs", "shaders/blur_fragment.fs"};
+    if(!blurShader.getBuildSuccess()) {
         std::cout << "Oops, simple shader failed to load" << std::endl;
         close(context);
         return 1;
@@ -155,7 +155,7 @@ int main(int argc, char* argv[]) {
         objectShader.setAttribPointerF("normal", 3, 8, 5);
     glBindVertexArray(0);
 
-    colorsharpenShader.use();
+    blurShader.use();
     //Set up a VAO for a single upright square
     GLuint simpleVAO {};
     glGenVertexArrays(1, &simpleVAO);
@@ -199,11 +199,11 @@ int main(int argc, char* argv[]) {
             GL_STATIC_DRAW
         );
 
-        colorsharpenShader.enableAttribArray("position");
-        colorsharpenShader.setAttribPointerF("position", 2, 4, 0);
+        blurShader.enableAttribArray("position");
+        blurShader.setAttribPointerF("position", 2, 4, 0);
 
-        colorsharpenShader.enableAttribArray("textureCoord");
-        colorsharpenShader.setAttribPointerF("textureCoord", 2, 4, 2);
+        blurShader.enableAttribArray("textureCoord");
+        blurShader.setAttribPointerF("textureCoord", 2, 4, 2);
     glBindVertexArray(0);
 
     objectShader.use();
@@ -395,7 +395,7 @@ int main(int argc, char* argv[]) {
         // the default colour buffer
         glClearColor(1.f, 1.f, 1.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
-        colorsharpenShader.use();
+        blurShader.use();
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, textureColorBuffer);
         glDisable(GL_DEPTH_TEST);
